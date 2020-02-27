5a1f2d12629b398c43fb7710bf75a1b04644215d
Sayali Ballal <sayali.ballal@ps97.team>
2020-02-27 11:49:09 +0530

    Added changes

---
+++ src/test/java/Test/CBEOrderProcess.java
@@ -0,0 +1,124 @@
+package Test;
+
+import java.sql.Time;
+
+//import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import Webpages.LoginPage;
+
+public class CBEOrderProcess {
+	
+	WebDriver driver = null;
+	/*public static WebDriver driver;
+	public String baseUrl = "https://alpha.combinedbook.com/";
+	*/
+	
+	@BeforeTest
+public void setUpTest(){
+		
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\sayali.ballal\\eclipse-workspaceProjects\\CombinedBookExhibit\\Drivers\\chromedriver\\chromedriver_win32 (2)\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://alpha.combinedbook.com/");
+		driver.manage().window().maximize();
+	}
+	
+	/*@BeforeTest
+    public void beforeTest() {  
+       String exePath = "C:\\Users\\sayali.ballal\\eclipse-workspaceProjects\\CombinedBookExhibit\\Drivers\\chromedriver\\chromedriver_win32(1)\\chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver", exePath);
+        driver = new ChromeDriver();  
+        driver.get("https://alpha.combinedbook.com/");
+    } */
+	
+	@Test
+public void loginpage() {
+		
+		LoginPage.loginpage_username(driver).sendKeys("cbetest1@gmail.com");
+		System.out.println("Test Completed Successfully for username");
+		
+		LoginPage.loginpage_password(driver).sendKeys("123456789");
+		System.out.println("Test Completed Successfully for password");
+		
+		LoginPage.loginpage_loginbtn(driver).sendKeys(Keys.RETURN);
+		System.out.println("Test Completed Successfully for loginbtn");
+		
+		LoginPage.beginwithorder(driver).click();
+		System.out.println("Test Completed Successfully for begin with order");
+		
+		LoginPage.selectregisterforbookfairs(driver).click();	
+		System.out.println("Test Completed Successfully for selectregisterforbook");
+							
+		LoginPage.selectregister_continuebtn(driver).click();
+		System.out.println("Test Completed Successfully for continue button");	
+
+       LoginPage.show_checkbox3(driver).click();
+       System.out.println("Test Completed Successfully for select checkbox3");
+       
+       LoginPage.show_continuebtn(driver).click();
+       System.out.println("Test Completed Successfully for Show_countinebtn");    
+       
+       LoginPage.selectexistingtitle(driver).click();
+       System.out.println("Test Completed Successfully for Select Existing Title");  
+       
+       LoginPage.selecttitlefromddl(driver).click();
+       System.out.println("Test Completed Successfully for Select Existing Title from DDL");  
+       
+       LoginPage.titleTandC(driver).click();
+       System.out.println("Test Completed Successfully for Title page_Terms and Conditions");
+       
+       LoginPage.titlepagecontinuebtn(driver).click();
+       System.out.println("Test Completed Successfully for Title page_continue button");
+       /*
+       LoginPage.AddOnServices(driver).click();
+       System.out.println("Test Completed Successfully for Add-on Services");
+       */
+       /*
+       LoginPage.SelectNewsletterMonthAndYear(driver).click();
+       System.out.println("est Completed Successfully for Select Newsletter Month And Year");
+       */
+       LoginPage.ContinueRegistration(driver).click();
+       System.out.println("Test Completed Successfully for Continue Registration");
+       
+       LoginPage.PubDetailspage(driver).click();
+       System.out.println("Test Completed Successfully for checkbox selection of 'Same As Publisher Details'");
+       
+       LoginPage.OrderPreviewAndConfirmProcessOrder(driver).click();
+       System.out.println("Test Completed Successfully for Process order");
+                 
+       
+       LoginPage.PreviewDetailsPage(driver).click();
+       System.out.println("Test Completed Successfully checkbox selection of 'I have reviewed the content and approve the information provided here.'");
+       
+       
+       LoginPage.PreviewDetailsContinueButton(driver).click();
+       System.out.println("Test Completed Successfully for Preview Details Continue button");
+       
+       
+       LoginPage.CreditCardNumber(driver).sendKeys("4111111111111111");
+       System.out.println("Test Completed Successfully entering for credit card number");
+       
+       LoginPage.CVV2(driver).sendKeys("123");
+       System.out.println("Test Completed Successfully for entering CVV2 number");
+       
+       LoginPage.ExpirationMonth(driver).click();
+       System.out.println("Test Completed Successfully for selecting expiration month");
+       
+	}
+	 
+	@AfterTest
+public void tearDownTest() {
+		
+		
+		driver.close();
+		driver.quit();
+		System.out.println("Test Completed Successfully...");
+	}	
+
+}
--- src/test/java/Webpages/LoginPage.java
+++ src/test/java/Webpages/LoginPage.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.WebElement;
 
 import net.bytebuddy.asm.Advice.Exit;
 
-public class LoginPage {
+public class LoginPage{
 	
 	private static WebElement element = null;
 	private static WebDriver checkbox;
@@ -42,41 +42,20 @@ public class LoginPage {
 	}
 	
 	
-	public static WebElement selectregisterforbook(WebDriver driver) {
-		element = driver.findElement(By.id("panelcollapseSix"));
-		return element;
-	}
-	
-	/*
-	public static WebElement selectregisterforbook_selectcheckbox(WebDriver driver) {
-	
-		element = driver.findElement(By.xpath("//label[@for='chkdtexhibittitle']"));
-		//element = driver.findElement(By.id("chkdtexhibittitle"));
+	public static WebElement selectregisterforbookfairs(WebDriver driver) {
+		element = driver.findElement(By.xpath("//*[@id=\"collapseSix\"]/div/div/div[1]/div[1]/div/div/ins"));
 		try {
-			Thread.sleep(8000);
+			Thread.sleep(3000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
-			
-			return element;
-		}*/
+			e.printStackTrace();}
 		
-	public static WebElement selectregisterforbook_selectcheckbox(WebDriver driver) {
-	        List<WebElement> checkbox = driver.findElements(By.xpath("//label[@for='chkdtexhibittitle']"));
-	        ((WebElement) checkbox.get(0)).click();
-	        
-	        try {
-				Thread.sleep(8000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();}
-					
-	        return element;	
+		return element;
 	}
-	
-		
+			
 	public static WebElement selectregister_continuebtn(WebDriver driver) {
-		element = driver.findElement(By.id("disp_type_btn"));
+		//element = driver.findElement(By.id("disp_type_btn"));
+		element = driver.findElement(By.xpath("//*[@id=\"disp_type_btn\"]"));
 		
 		try {
 			Thread.sleep(8000);
@@ -87,48 +66,208 @@ public class LoginPage {
 		return element;	
 	}
 	
-	
-	public static WebElement select_packages_shows(WebDriver driver)
-	{
-		element = driver.findElement(By.id("panelcollapseTwo"));
-		
+	public static WebElement show_checkbox3(WebDriver driver) {
+		element = driver.findElement(By.xpath("//*[@id=\"select_packages_shows_tbfirst\"]/tbody/tr[3]/td[3]/div/ins"));
+		//element.click();
+				
 		try {
-			Thread.sleep(8000);
+			Thread.sleep(3000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();}
 		
 		return element;
+	}
 		
-	  } 
+	public static WebElement show_continuebtn(WebDriver driver) {
+		    element = driver.findElement(By.xpath("//*[@id=\"shows_btn\"]"));
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();}
+			
+			return element;
+		}
+			
+	public static WebElement selectexistingtitle(WebDriver driver) {
+		    element = driver.findElement(By.xpath("//*[@id=\"cmbBook1\"]")); 
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();}
+			
+			return element;
+		}
+		
+	public static WebElement selecttitlefromddl(WebDriver driver) {
+			element = driver.findElement(By.xpath("//*[@id=\"cmbBook1\"]/option[13]"));
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();}
+			
+			return element;
+		}
+	
+	public static WebElement titleTandC(WebDriver driver) {
+		element = driver.findElement(By.xpath("//*[@id=\"collapseFour\"]/div/div[2]/div/div[3]/div/div/table/tbody/tr/td/div/ins"));
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();}
+		
+		return element;
+	}
+	
+	public static WebElement titlepagecontinuebtn(WebDriver driver) {
+		element = driver.findElement(By.xpath("//*[@id=\"titile_next\"]"));
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();}
+		
+		return element;
+	}
+	/*// AddOn services, it's a optional 
+	public static WebElement AddOnServices(WebDriver driver) {
+		element = driver.findElement(By.xpath("//*[@id=\"div_featured_title\"]/div[1]/span/div/ins"));
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();}
+		
+		return element;
+	}
+	*/
+	/*//SelectNewsletterMonthAndYear it's an optional 
+	public static WebElement SelectNewsletterMonthAndYear(WebDriver driver) {
+		element = driver.findElement(By.xpath("//*[@id=\"chkTBCNewsletter_listing\"]/div/table/tbody/tr/td[2]/div[4]/div/ins"));
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();}
+		
+		return element;
+	}
+	*/
+	
+	public static WebElement ContinueRegistration(WebDriver driver) {
+		element = driver.findElement(By.xpath("//*[@id=\"addon_next\"]"));
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();}
+		
+		return element;
+	}
+	
+	public static WebElement PubDetailspage(WebDriver driver) {
+		element = driver.findElement(By.xpath("//*[@id=\"Register_main\"]/div[2]/div/div[2]/div[3]/div/ins"));
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();}
+		
+		return element;
+	}
+	
+	public static WebElement OrderPreviewAndConfirmProcessOrder(WebDriver driver) {
+		element = driver.findElement(By.xpath("//*[@id=\"SubmitBtn\"]"));
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();}
+		
+		return element;
+	}
+	
+	public static WebElement PreviewDetailsPage(WebDriver driver) {
+		element = driver.findElement(By.xpath("//*[@id=\"online_catalogs\"]/div[4]/div/ins"));
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();}
+		
+		return element;
+	}
+	
+	
+	public static WebElement PreviewDetailsContinueButton(WebDriver driver) {
+		element = driver.findElement(By.xpath("//*[@id=\"titile_next\"]"));
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();}
+		
+		return element;
+	}
+	
+	public static WebElement CreditCardNumber(WebDriver driver) {
+		element = driver.findElement(By.xpath("//*[@id=\"CC_NUM\"]"));
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();}
+		
+		return element;
+	}
+	
+	public static WebElement CVV2(WebDriver driver) {
+		element = driver.findElement(By.xpath("//*[@id=\"CVCCVV2\"]"));
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();}
+		
+		return element;
+	}
+	
+	public static WebElement ExpirationMonth(WebDriver driver) {
+		element = driver.findElement(By.xpath("//*[@id=\"CC_EXPIRES\"]/option[01]"));
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();}
+		
+		return element;
+	}
+	
+	
+	
+		
+	}
 
-	public static WebElement  Pick_Individual_Shows(WebDriver driver){
-	
-		element = driver.findElement(By.xpath("//button[@class='blue_line pull-left btn_toggle line_sep_space']"));
-		
-		try {
-			Thread.sleep(8000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();}
-		
-		return element;
-	}
-	
-	public static WebElement show_checkbox(WebDriver driver) {
-		//element = driver.findElement(By.id("show_651"));
-		//element = driver.findElement(By.xpath("//input[@id='show_651']"));
-		List<WebElement> checkbox = driver.findElements(By.xpath("//input[@id='mreg_651_print_title']"));
-        ((WebElement) checkbox.get(0)).click();
-		
-		try {
-			Thread.sleep(8000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();}
-		
-		return element;
-	}
-	
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
